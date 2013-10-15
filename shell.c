173dc950bb62b12d133902ea3dc3643b4cb5a43f
Tim <tim37021@gmail.com>
2013-10-15 14:33:20 +0800

    Add filedump function, and cat command.

--- shell.c
+++ shell.c
@@ -36,6 +36,35 @@ void ls_command(int n, char *argv[]){
 
 }
 
+int filedump(const char *filename){
+        char buf[128];
+
+        int fd=fs_open(filename, 0, O_RDONLY);
+
+        if(fd==OPENFAIL)
+                return 0;
+
+        fio_printf(1, "\r\n");
+
+        int count;
+        while((count=fio_read(fd, buf, sizeof(buf)))>0){
+                fio_write(1, buf, count);
+        }
+
+        fio_close(fd);
+	return 1;
+}
+
+void cat_command(int n, char *argv[]){
+	if(n==1){
+		fio_printf(2, "\r\nUsage: cat <filename>\r\n");
+		return;
+	}
+
+	if(!filedump(argv[1]))
+		fio_printf(2, "\r\n%s no such file or directory.\r\n", argv[1]);
+}
+
 void man_command(int n, char *argv[]){
 	if(n==1){
 		fio_printf(2, "\r\nUsage: man <command>\r\n");
@@ -45,27 +74,15 @@ void man_command(int n, char *argv[]){
 	char buf[128]="/romfs/manual/";
 	strcat(buf, argv[1]);
 
-	int fd=fs_open(buf, 0, O_RDONLY);
-
-	if(fd==OPENFAIL){
-		fio_printf(2, "\r\nManual not available\r\n");
-		return;
-	}
-
-	fio_printf(1, "\r\n");
-
-	int count;
-	while((count=fio_read(fd, buf, sizeof(buf)))>0){
-		fio_write(1, buf, count);
-	}
-
-	fio_close(fd);
+	if(!filedump(buf))
+		fio_printf(2, "\r\nManual not available.\r\n");
 }
 
 cmdfunc *do_command(const char *cmd){
 	static cmdlist cl[]={
 		{.name="ls", .fptr=ls_command, .desc="List directory"}
 		,{.name="man", .fptr=man_command, .desc="Show the manual of the command"}
+		,{.name="cat", .fptr=cat_command, .desc="concatenate files and print on the stdout"}
 	};
 
 	int i;
