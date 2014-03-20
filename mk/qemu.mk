8c669c58206be04b3df36128d89b182eeb160e70
Rampant <rampant1018@gmail.com>
2014-03-20 10:39:02 +0800

    Fix: Missing qemu parameter.

--- mk/qemu.mk
+++ mk/qemu.mk
@@ -4,7 +4,8 @@ BUILD_TARGET = $(OUTDIR)/$(TARGET)
 qemu: $(BUILD_TARGET).bin $(QEMU_STM32)
 	$(QEMU_STM32) -M stm32-p103 \
 	    -monitor stdio \
-	    -kernel $(BUILD_TARGET).bin
+	    -kernel $(BUILD_TARGET).bin \
+	    -semihosting
 
 qemudbg: $(BUILD_TARGET).bin $(QEMU_STM32)
 	$(QEMU_STM32) -M stm32-p103 \
