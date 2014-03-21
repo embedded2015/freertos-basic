68bddb935d1f62bfd3f8ef34ac603465f2f26f1d
Rampant <rampant1018@gmail.com>
2014-03-21 13:56:05 +0800

    gdb: Modify qemudbg. Now will use gdbtui and run in same window.

--- mk/qemu.mk
+++ mk/qemu.mk
@@ -1,5 +1,6 @@
 QEMU_STM32 ?= ../qemu_stm32/arm-softmmu/qemu-system-arm
 BUILD_TARGET = $(OUTDIR)/$(TARGET)
+export SHELL := /bin/bash
 
 qemu: $(BUILD_TARGET).bin $(QEMU_STM32)
 	$(QEMU_STM32) -M stm32-p103 \
@@ -9,5 +10,7 @@ qemu: $(BUILD_TARGET).bin $(QEMU_STM32)
 
 qemudbg: $(BUILD_TARGET).bin $(QEMU_STM32)
 	$(QEMU_STM32) -M stm32-p103 \
+	    -monitor stdio \
 	    -gdb tcp::3333 -S \
-	    -kernel $(BUILD_TARGET).bin -semihosting
+	    -kernel $(BUILD_TARGET).bin -semihosting 2>&1>/dev/null & \
+	    $(CROSS_COMPILE)gdbtui -x $(TOOLDIR)/gdbscript
